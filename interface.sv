a53d859edd444bc52f57b4ec81590f5768ea7cd8
VLSIwithNayana <bhayaninayana1913@gmail.com>
2026-06-27 12:01:41 +0530

    Define dff_if interface with clock and reset signals

---
+++ interface.sv
@@ -0,0 +1,8 @@
+// Define an interface "dff_if" with the following signals
+interface dff_if;
+  logic clk;   // Clock signal
+  logic rst;   // Reset signal
+  logic din;   // Data input
+  logic dout;  // Data output
+  
+endinterface
